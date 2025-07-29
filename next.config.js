1ad30825c3b166100fd9eefcd26cc2c4c6d701da
jihyun <jihyun3066@naver.com>
2025-07-29 23:29:28 +0900

    배포 시 검사 건너뜀

--- next.config.js
+++ next.config.js
@@ -3,11 +3,19 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'skillicons.dev',
+        protocol: "https",
+        hostname: "skillicons.dev",
       },
     ],
   },
+  eslint: {
+    // 빌드 중에 ESLint 검사를 건너뜁니다
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // 빌드 시 타입 체크를 실행하지 않음
+    ignoreBuildErrors: true,
+  },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
