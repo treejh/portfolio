f2b4d04086188ea0f02ecc4fcff02779b65f6db7
jihyun <jihyun3066@naver.com>
2025-08-10 21:10:14 +0900

    수정

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -55,7 +55,7 @@ export default function Certificates() {
                   <h3 className="text-xl font-bold text-gray-800">
                     {cert.title}
                   </h3>
-                  <span className="inline-flex items-center gap-2 px-4 py-2 bg-blue-100 rounded-full">
+                  <span className="inline-flex items-center gap-2 ">
                       <svg className="w-4 h-4 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                       </svg>
